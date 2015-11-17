c4abb350d725682520d56d4847a7117e7bb4f1bd
Jon-Carlos Rivera <jon.carlos.rivera@gmail.com>
2015-11-17 10:23:19 -0500

    Used async to make the fetch process throttle-able Fixed manifest generation so that it is now aware of the future location of sub-playlists

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ var path = require('path');
 var mkdirp = require('mkdirp');
 var fetch = require('fetch');
 var parseManifest = require('./parse.js');
+var async = require('async');
+
+var CONCURRENT_FETCH_LIMIT = 5;
 
 function getCWDName (parentUri, localUri) {
   // Do I need to use node's URL object?
@@ -26,48 +29,80 @@ function getCWDName (parentUri, localUri) {
   return localPaths.slice(i + 1).join('_');
 }
 
-function getIt (cwd, uri) {
+function createManifestText (manifest, rootUri) {
+  return manifest.map(function (line) {
+    if (line.type === 'playlist') {
+      var subCWD = getCWDName(rootUri, line.line);
+      return subCWD + '/' + path.basename(line.line);
+    } else if (line.type === 'segment') {
+      return path.basename(line.line);
+    }
+    return line.line;
+  }).join('\n');
+}
+
+function getIt (cwd, uri, done) {
   var playlistFilename = path.basename(uri);
 
   // Fetch playlist
   fetch.fetchUrl(uri, function getPlaylist (err, meta, body) {
     if (err) {
       // TODO: Error handling? reporting?
-      return;
+      return done(err);
     }
 
-    //   Parse playlist
+    // Parse playlist
     var manifest = parseManifest(uri, body.toString());
+
     // Save manifest
-    fs.writeFileSync(path.resolve(cwd, playlistFilename), manifest.localManifest);
+    fs.writeFileSync(path.resolve(cwd, playlistFilename), createManifestText(manifest, uri));
 
-    console.log(manifest);
-    //   For each resource in manifest.resources
-    manifest.resources.forEach(function (resource) {
-      if (resource.type === 'segment') {
-        var filename = path.basename(resource.uri);
-
-        // Fetch it to CWD (streaming)
-        var segmentStream = new fetch.FetchStream(resource.uri);
-        var outputStream = fs.createWriteStream(path.resolve(cwd, filename));
-
-        segmentStream.pipe(outputStream);
-        // TODO: Error handling? reporting?
-      } else {
-        // Create subCWD from URI
-        var subCWD = getCWDName(uri, resource.uri);
-        // If subCWD does not exist, make subCWD (mkdirp)
-        mkdirp(path.resolve(subCWD), function (err) {
-            if (err) {
-              // TODO: Error handling? reporting?
-              return;
-            }
-
-            // Call `getIt` with subCWD and resource uri
-            getIt(subCWD, resource.uri);
-        });
-      }
+    var segments = manifest.filter(function (resource) {
+      return resource.type === 'segment';
     });
+    var playlists = manifest.filter(function (resource) {
+      return resource.type === 'playlist';
+    });
+
+    async.series([
+      function fetchSegments (next) {
+        async.eachLimit(segments, CONCURRENT_FETCH_LIMIT, function (resource, done) {
+          var filename = path.basename(resource.line);
+
+          console.log('Start fetching', resource.line);
+
+          // Fetch it to CWD (streaming)
+          var segmentStream = new fetch.FetchStream(resource.line);
+          var outputStream = fs.createWriteStream(path.resolve(cwd, filename));
+
+          segmentStream.pipe(outputStream);
+          segmentStream.on('error', function (err) {
+            console.log('Fetching of', resource.line, 'failed with error:', err);
+            return done(err);
+          });
+          segmentStream.on('end', function () {
+            console.log('Finished fetching', resource.line);
+            return done();
+          });
+        }, next);
+      },
+      function fetchPlaylists (next) {
+        async.eachSeries(playlists, function (resource, done) {
+          // Create subCWD from URI
+          var subCWD = getCWDName(uri, resource.line);
+          // If subCWD does not exist, make subCWD (mkdirp)
+          mkdirp(path.resolve(subCWD), function (err) {
+              if (err) {
+                // TODO: Error handling? reporting?
+                return done(err);
+              }
+
+              // Call `getIt` with subCWD and resource uri
+              getIt(subCWD, resource.line, done);
+          });
+        }, next);
+      }
+    ], done);
   });
 }
 
--- output/test.js
+++ output/test.js
@@ -1,3 +1,9 @@
 var getIt = require('../');
 
-getIt('./', 'https://devimages.apple.com.edgekey.net/streaming/examples/bipbop_4x3/bipbop_4x3_variant.m3u8');
+getIt('./', 'https://devimages.apple.com.edgekey.net/streaming/examples/bipbop_4x3/bipbop_4x3_variant.m3u8', function (err) {
+  if (err) {
+    console.log('IT ALL BROKE!');
+    return;
+  }
+  console.log('HLS saved!');
+});
