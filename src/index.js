ce8814bdeeb63a16494c42246d6fea59a453a008
gsingh <gsingh@brightcove.com>
2018-06-20 20:42:32 -0400

    First take on making hls async;

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ var WriteData = require('./write-data');
 
 var main = function(options) {
   console.log("Gathering Manifest data...");
-  var resources = WalkManifest(options.decrypt, options.output, options.input);
-
-  return WriteData(options.decrypt, options.concurrency, resources);
+  var resources = [];
+  WalkManifest(options.decrypt, options.output, options.input, false, 0, resources, function(){
+    return WriteData(options.decrypt, options.concurrency, resources);
+  });
 };
 
 module.exports = main;
