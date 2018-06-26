b72c83cd02e2ce47c161c83f8306a22cf375f9f2
gsingh <gsingh@brightcove.com>
2018-06-26 11:09:41 -0400

    Convert index to use options;

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ var WriteData = require('./write-data');
 
 var main = function(options, callback) {
   console.log("Gathering Manifest data...");
-  WalkManifest(options.decrypt, options.output, options.input, function(err, resources) {
+  var options = {decrypt: options.decrypt, basedir: options.output, uri: options.input};
+  WalkManifest(options, function(err, resources) {
     console.log("Downloading additional data...");
     WriteData(options.decrypt, options.concurrency, resources, callback);
   });
