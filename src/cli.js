d0bfdc63ad131a6afaeb14ad863f1fdd4633103f
gsingh <gsingh@brightcove.com>
2018-06-27 01:11:30 -0400

    Some final cleanup;

--- src/cli.js
+++ src/cli.js
@@ -2,20 +2,20 @@
 var path = require('path');
 var start = require('./index');
 var pessimist = require('pessimist')
-  .usage('Fetch and save the contents of an HLS playlist locally.\nUsage: $0 ')
-  .alias('i', 'input')
-  .demand('i')
-  .describe('i', 'uri to m3u8 (required)')
-  .alias('o', 'output')
-  .default('o', './hls-fetcher')
-  .describe('o', "output path (default:'./hls-fetcher')")
-  .alias('c', 'concurrency')
-  .default('c', Infinity)
-  .describe('c', 'number of simultaneous fetches (default: Infinity)')
-  .alias('d', 'decrypt')
-  .default('d', false)
-  .describe('d', 'decrypt and remove enryption from manifest (default: false)')
-  .argv;
+    .usage('Fetch and save the contents of an HLS playlist locally.\nUsage: $0 ')
+    .alias('i', 'input')
+    .demand('i')
+    .describe('i', 'uri to m3u8 (required)')
+    .alias('o', 'output')
+    .default('o', './hls-fetcher')
+    .describe('o', "output path (default:'./hls-fetcher')")
+    .alias('c', 'concurrency')
+    .default('c', Infinity)
+    .describe('c', 'number of simultaneous fetches (default: Infinity)')
+    .alias('d', 'decrypt')
+    .default('d', false)
+    .describe('d', 'decrypt and remove enryption from manifest (default: false)')
+    .argv;
 
 // Make output path
 var output = path.resolve(pessimist.o);
