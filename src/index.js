bdce7b07d51155554404a7a4165747ce8115595c
gsingh <gsingh@brightcove.com>
2018-07-11 23:27:50 -0400

    Return promise; No Catch Block; Better message;

--- src/index.js
+++ src/index.js
@@ -4,14 +4,11 @@ var WriteData = require('./write-data');
 var main = function(options) {
   console.log("Gathering Manifest data...");
   var options = {decrypt: options.decrypt, basedir: options.output, uri: options.input};
-  WalkManifest(options)
+  return WalkManifest(options)
     .then(function(resources) {
       console.log("Downloading additional data...");
       return WriteData(options.decrypt, options.concurrency, resources);
     })
-    .catch(function(err) {
-      throw err;
-    });
 };
 
 module.exports = main;
